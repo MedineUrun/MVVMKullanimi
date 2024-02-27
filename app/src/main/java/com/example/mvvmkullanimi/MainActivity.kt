27cc7d074cc55e1c1cfc4e203aab67c16e4fbb35
MedineUrun <medine_urun@hotmail.com>
2024-02-27 21:00:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mvvmkullanimi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.mvvmkullanimi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel:MainActivityViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainActivityNesnesi = this //bu nesne olmadan aşağıdaki fonksiyonlara erişemeyiz(databinding özelliği)
+
+        viewModel.sonuc.observe(this){
+            binding.hesaplamaSonucu = it
+        }
+    }
+
+    fun topla(alinanSayi1:String, alinanSayi2:String){
+        viewModel.topla(alinanSayi1,alinanSayi2) //işlem yapıyor
+    }
+
+    fun carp(alinanSayi1:String, alinanSayi2:String){
+        viewModel.carp(alinanSayi1,alinanSayi2) //işlem yapıyor
+    }
+}
